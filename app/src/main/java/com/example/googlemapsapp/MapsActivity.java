ba4e880c62e4a4f2d305a624a57547e2fcf5fbdd
Daveywilson <wilson-d31@ulster.ac.uk>
2019-11-28 16:54:41 +0000

    satellite view

--- app/src/main/java/com/example/googlemapsapp/MapsActivity.java
+++ app/src/main/java/com/example/googlemapsapp/MapsActivity.java
@@ -43,5 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
     }
 }
